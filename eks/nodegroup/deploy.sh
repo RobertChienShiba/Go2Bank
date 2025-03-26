53eb7f4837c05bab8a59a593d69a677027bf2be1
RobertChienShiba <robertmadhead0919@gmail.com>
2025-03-26 17:24:22 +0800

    :green_heart:fixed eksctl cmd install path

--- eks/nodegroup/deploy.sh
+++ eks/nodegroup/deploy.sh
@@ -1,3 +1,3 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.0/deploy/static/provider/aws/deploy.yaml
-kubectl apply -f https://raw.githubusercontent.com/external-secrets/external-secrets/main/deploy/crds/bundle.yaml
-kubectl apply -f .
+kubectl apply -f https://raw.githubusercontent.com/external-secrets/external-secrets/main/deploy/crds/bundle.yaml 
+kubectl apply -f eks/nodegroup/
